f6ece200eda30653d5f4588f10b1834f9d5f6149
backup <backup@backup-pc>
2020-09-28 09:22:50 +0530

    junit

--- src/test/java/com/neosofttech/technologies/TechnologiesApplicationTests.java
+++ src/test/java/com/neosofttech/technologies/TechnologiesApplicationTests.java
@@ -1,5 +1,6 @@
 package com.neosofttech.technologies;
 
+import static com.google.common.base.CharMatcher.is;
 import com.neosofttech.technologies.controller.WebfluxRestController;
 import com.neosofttech.technologies.dto.Employee;
 import org.junit.jupiter.api.Test;
@@ -15,10 +16,12 @@ import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 import com.neosofttech.technologies.repository.EmployeeRepository;
 import com.neosofttech.technologies.service.EmployeeService;
+import static io.netty.util.internal.SystemPropertyUtil.get;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import javax.servlet.http.HttpServletResponse;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -31,12 +34,25 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.http.ResponseEntity.ok;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.web.context.WebApplicationContext;
+
 
 
 @SpringBootTest
@@ -45,20 +61,40 @@ class TechnologiesApplicationTests {
         @Test
         void contextLoads() {
         }
+        
+        private MockMvc mockMvc;
+        
+         @Autowired
+         private WebApplicationContext wac;
 
+        
+ 
 	@MockBean
 	private EmployeeService service;
         
-        @Mock
-        EmployeeRepository mockRepository;
-                   
-        @Test
-	public void testFindAllEmployee() {
+         @Mock
+         EmployeeRepository mockRepository;
+        
+         @BeforeEach
+         public void setup() {
+         DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+         this.mockMvc = builder.build();         }
+         
+         @Test
+	 public void testAddEmployee() throws Exception {
              Employee emplyoee = new Employee(1,"vikash");
              when(mockRepository.save(any(Employee.class))).thenReturn(emplyoee);
-                     assertEquals(emplyoee.getId(), 1);
-
+             assertEquals(emplyoee.getId(), 1);
+             ResultMatcher ok = MockMvcResultMatchers.status().isOk();
+             MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/Webflux/getAllEmplyoee");
+             this.mockMvc.perform(builder).andDo(print())
+                    .andExpect(ok);
+         }
+        
+        
 	}
+        
+       
 
       
- }
+
